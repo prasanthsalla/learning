d2b6453a60b15b527fd6dd5719bb736a906ee86d
Manjunath Govindaraju <manjunath.govindaraju@philips.com>
2018-06-25 00:05:35 -0700

    some fix

--- src/main/java/org/arna/log/LogController.java
+++ src/main/java/org/arna/log/LogController.java
@@ -11,21 +11,30 @@ import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 @RestController
-@RequestMapping(value = "/log", produces = {
-        MediaType.APPLICATION_JSON_VALUE
-})
 public class LogController {
-
-    @PostMapping
+    @RequestMapping(
+            method=RequestMethod.POST,
+            value="/log",
+            consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<?> publish (@RequestBody LogMessage contents) {
         System.out.print(contents.getDate() + contents.getMessage());
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping
+    @RequestMapping(
+            method=RequestMethod.GET,
+            value="/log",
+            consumes = MediaType.APPLICATION_JSON_VALUE)
     LogMessage getlog() {
         Date date = new Date();
         return new LogMessage(date, "Test Message");
+    }
+
+    @RequestMapping(
+            method=RequestMethod.GET,
+            value="/status")
+    public ResponseEntity<Object> healthCheck() {
+        return new ResponseEntity<>("Service is up and running", null, HttpStatus.OK);
     }
 
 }
